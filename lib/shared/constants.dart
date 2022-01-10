597ea65ae93901ea34e9575353b35749b34413cd
Othniel Ussher <othnielussher16@gmail.com>
2022-01-10 20:44:35 +0000

    🌠 Add favicon asset

---
+++ lib/shared/constants.dart
@@ -0,0 +1,12 @@
+class Images {
+  static const String imagesPath = 'assets/images/';
+  static const String logoIconWhite = imagesPath + 'logo_icon_white.png';
+  static const String logoTextWhite = imagesPath + 'logo_text_white.png';
+  static const String logoTextBlack = imagesPath + 'logo_icon_black.png';
+  static const String woman = imagesPath + 'woman.jpg';
+}
+
+class Fonts {
+  static const String mazzard = 'Mazzard';
+  static const String autography = 'Autography';
+}
