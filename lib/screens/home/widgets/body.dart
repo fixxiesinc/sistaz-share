f9a2e2ae36f0ca7ddf490a406080d06562147cb8
Othniel Ussher <othnielussher16@gmail.com>
2022-01-10 20:46:45 +0000

    Project setup

---
+++ lib/screens/home/widgets/body.dart
@@ -0,0 +1,64 @@
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+import 'package:sistaz_share_web/exports.dart';
+
+class Body extends StatelessWidget {
+  final Widget child;
+  const Body({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        final bool isMobile = sizingInformation.isMobile;
+        final bool isDesktop = sizingInformation.isDesktop;
+        final bool isTablet = sizingInformation.isTablet;
+        return Padding(
+          padding: EdgeInsets.symmetric(horizontal: isMobile ? 20.0 : 40.0),
+          child: Flex(
+            direction: isDesktop ? Axis.horizontal : Axis.vertical,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: isDesktop
+                ? MainAxisAlignment.spaceBetween
+                : MainAxisAlignment.start,
+            children: [
+              Flexible(
+                child: child,
+                flex: isMobile
+                    ? 3
+                    : isTablet
+                        ? 6
+                        : 5,
+              ),
+              SizedBox(
+                width: isDesktop ? size.width * 0.1 : 0.0,
+                height: isMobile
+                    ? size.height * 0.1
+                    : isTablet
+                        ? size.width * 0.15
+                        : 0.0,
+              ),
+              Expanded(
+                flex: isMobile
+                    ? 7
+                    : isTablet
+                        ? 8
+                        : 3,
+                child: Transform(
+                  alignment: FractionalOffset.center,
+                  transform: Matrix4.rotationY(math.pi),
+                  child: Image.asset(
+                    Images.woman,
+                    width: isDesktop ? size.width * 0.3 : size.width,
+                    height: isDesktop ? size.width * 0.3 : size.width,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
