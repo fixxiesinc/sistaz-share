74a5d95174cff27a4a0b7c51880642d6ae6b6938
Othniel Ussher <othnielussher16@gmail.com>
2022-01-10 20:46:38 +0000

    Project setup

---
+++ lib/screens/splash.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:sistaz_share_web/exports.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  bool showHome = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 4000)).then((value) {
+      setState(() {
+        showHome = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: showHome
+          ? const DelayedDisplay(
+              child: Home(),
+              slidingBeginOffset: Offset(0, 0),
+              fadingDuration: Duration(milliseconds: 500),
+            )
+          : Center(
+              child:
+                  Image.asset(Images.logoIconWhite, height: 70.0, width: 70.0),
+            ),
+    );
+  }
+}
