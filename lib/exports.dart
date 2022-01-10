83c22cb7ba16d820016af3bc5cf17fd6b00d64e7
Othniel Ussher <othnielussher16@gmail.com>
2022-01-10 20:46:32 +0000

    Project setup

---
+++ lib/exports.dart
@@ -0,0 +1,15 @@
+// pub
+export 'package:dots_indicator/dots_indicator.dart';
+export 'package:delayed_display/delayed_display.dart';
+export 'package:responsive_builder/responsive_builder.dart';
+
+// local
+export 'package:sistaz_share_web/app.dart';
+export 'package:sistaz_share_web/widgets/footer.dart';
+export 'package:sistaz_share_web/screens/splash.dart';
+export 'package:sistaz_share_web/shared/constants.dart';
+export 'package:sistaz_share_web/screens/home/home.dart';
+export 'package:sistaz_share_web/screens/home/widgets/body.dart';
+export 'package:sistaz_share_web/widgets/social_media_buttons.dart';
+export 'package:sistaz_share_web/screens/home/widgets/first_body.dart';
+export 'package:sistaz_share_web/screens/home/widgets/second_body.dart';
