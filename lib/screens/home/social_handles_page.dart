94ee54311ce22e5b900994dd64c71a05489ab850
Othniel Ussher <othnielussher16@gmail.com>
2022-01-15 20:22:19 +0000

    Rewrite second body UI

--- lib/screens/home/social_handles_page.dart
+++ lib/screens/home/social_handles_page.dart
@@ -7,27 +7,24 @@ class SocialHandles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SocialHandleItem(
-            label: 'Twitter',
-            onTap: () => dartjs.context.callMethod('open', [Urls.twitter]),
-          ),
-          const SizedBox(height: 30.0),
-          SocialHandleItem(
-            label: 'Instagram',
-            onTap: () => dartjs.context.callMethod('open', [Urls.instagram]),
-          ),
-          const SizedBox(height: 30.0),
-          SocialHandleItem(
-            label: 'Facebook',
-            onTap: () => dartjs.context.callMethod('open', [Urls.facebook]),
-          ),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SocialHandleItem(
+          label: 'Instagram',
+          onTap: () => dartjs.context.callMethod('open', [Urls.instagram]),
+        ),
+        const SizedBox(height: 30.0),
+        SocialHandleItem(
+          label: 'Facebook',
+          onTap: () => dartjs.context.callMethod('open', [Urls.facebook]),
+        ),
+        const SizedBox(height: 30.0),
+        SocialHandleItem(
+          label: 'Twitter',
+          onTap: () => dartjs.context.callMethod('open', [Urls.twitter]),
+        ),
+      ],
     );
   }
 }
