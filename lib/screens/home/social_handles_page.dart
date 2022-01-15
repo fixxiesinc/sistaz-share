6b82c6f6227bfbe909172e19073a4d90a163fb91
Othniel Ussher <othnielussher16@gmail.com>
2022-01-15 16:43:52 +0000

    Add ontap events

--- lib/screens/home/social_handles_page.dart
+++ lib/screens/home/social_handles_page.dart
@@ -1,26 +1,33 @@
+import 'dart:js' as dartjs;
 import 'package:flutter/material.dart';
+import 'package:sistaz_share_web/exports.dart';
 
 class SocialHandles extends StatelessWidget {
   const SocialHandles({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(vertical: 20.0),
-          decoration: BoxDecoration(
-            color: Colors.transparent,
-            border: Border.all(color: Colors.white),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SocialHandleItem(
+            label: 'Twitter',
+            onTap: () => dartjs.context.callMethod('open', [Urls.twitter]),
           ),
-          child: const Text(
-            'Twitter',
-            style: TextStyle(color: Colors.white, fontSize: 18.0),
+          SizedBox(height: 30.0),
+          SocialHandleItem(
+            label: 'Instagram',
+            onTap: () => dartjs.context.callMethod('open', [Urls.instagram]),
           ),
-        ),
-      ],
+          SizedBox(height: 30.0),
+          SocialHandleItem(
+            label: 'Facebook',
+            onTap: () => dartjs.context.callMethod('open', [Urls.facebook]),
+          ),
+        ],
+      ),
     );
   }
 }
