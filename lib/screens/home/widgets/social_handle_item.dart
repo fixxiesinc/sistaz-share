ea141327555db51dc2e22f3ea36f18e0ec4cec14
Othniel Ussher <othnielussher16@gmail.com>
2022-01-15 16:44:10 +0000

    Create widget for social media handle items

---
+++ lib/screens/home/widgets/social_handle_item.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SocialHandleItem extends StatelessWidget {
+  final String label;
+  final VoidCallback onTap;
+  const SocialHandleItem({Key? key, required this.label, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.5,
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(vertical: 20.0),
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          border: Border.all(color: Colors.white),
+        ),
+        child: Text(
+          label,
+          style: const TextStyle(color: Colors.white, fontSize: 18.0),
+        ),
+      ),
+    );
+  }
+}
