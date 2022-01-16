8e6f30f4d50d851dff8c8eb44b9e6dcdeb1f821b
Othniel Ussher <othnielussher16@gmail.com>
2022-01-16 13:56:13 +0000

    🚢 Add new exports

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,5 +1,4 @@
 // pub
-export 'package:dots_indicator/dots_indicator.dart';
 export 'package:delayed_display/delayed_display.dart';
 export 'package:responsive_builder/responsive_builder.dart';
 
@@ -9,8 +8,9 @@ export 'package:sistaz_share_web/widgets/footer.dart';
 export 'package:sistaz_share_web/screens/splash.dart';
 export 'package:sistaz_share_web/shared/constants.dart';
 export 'package:sistaz_share_web/screens/home/home.dart';
-export 'package:sistaz_share_web/screens/home/widgets/first_body.dart';
+export 'package:sistaz_share_web/screens/home/widgets/header.dart';
 export 'package:sistaz_share_web/widgets/social_media_buttons.dart';
+export 'package:sistaz_share_web/screens/home/widgets/first_body.dart';
 export 'package:sistaz_share_web/screens/home/widgets/first_body.dart';
 export 'package:sistaz_share_web/screens/home/social_handles_page.dart';
 export 'package:sistaz_share_web/screens/home/widgets/second_body.dart';
