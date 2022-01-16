df93a766735a99dcf8d96d090e9de9c10f01d15b
Othniel Ussher <othnielussher16@gmail.com>
2022-01-16 15:07:07 +0000

    🚢 Update exports

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,17 +1,18 @@
 // pub
 export 'package:delayed_display/delayed_display.dart';
 export 'package:responsive_builder/responsive_builder.dart';
+export 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
 
 // local
 export 'package:sistaz_share_web/app.dart';
 export 'package:sistaz_share_web/widgets/footer.dart';
 export 'package:sistaz_share_web/screens/splash.dart';
 export 'package:sistaz_share_web/shared/constants.dart';
+export 'package:sistaz_share_web/screens/home/menu.dart';
 export 'package:sistaz_share_web/screens/home/home.dart';
+export 'package:sistaz_share_web/screens/home/social_handles.dart';
 export 'package:sistaz_share_web/screens/home/widgets/header.dart';
-export 'package:sistaz_share_web/widgets/social_media_buttons.dart';
 export 'package:sistaz_share_web/screens/home/widgets/first_body.dart';
 export 'package:sistaz_share_web/screens/home/widgets/first_body.dart';
-export 'package:sistaz_share_web/screens/home/social_handles_page.dart';
 export 'package:sistaz_share_web/screens/home/widgets/second_body.dart';
 export 'package:sistaz_share_web/screens/home/widgets/social_handle_item.dart';
