5cd41d407f56d19a3fd4b02fd715272dfc06004b
Othniel Ussher <othnielussher16@gmail.com>
2022-01-16 13:57:44 +0000

    Redesign pages

--- lib/screens/home/widgets/second_body.dart
+++ lib/screens/home/widgets/second_body.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:sistaz_share_web/exports.dart';
 
@@ -11,87 +12,70 @@ class SecondBody extends StatelessWidget {
       builder: (context, sizingInformation) {
         final bool isMobile = sizingInformation.isMobile;
         final bool isTablet = sizingInformation.isTablet;
-        final bool isDesktop = sizingInformation.isDesktop;
+        final _textStyle = TextStyle(
+          wordSpacing: 1.5,
+          color: Colors.orange,
+          fontFamily: Fonts.mazzard,
+          fontSize: isMobile
+              ? 16.0
+              : isTablet
+                  ? size.width * 0.02
+                  : size.width * 0.014,
+        );
         return Padding(
-          padding: EdgeInsets.symmetric(horizontal: isMobile ? 20.0 : 40.0),
-          child: Stack(
-            children: [
-              Flex(
-                mainAxisSize: MainAxisSize.min,
-                direction: isDesktop ? Axis.horizontal : Axis.vertical,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: isMobile
-                    ? MainAxisAlignment.start
-                    : MainAxisAlignment.spaceBetween,
-                children: [
-                  Flexible(
-                    flex: isMobile
-                        ? 0
+          padding: EdgeInsets.symmetric(horizontal: isMobile ? 20.0 : 60.0),
+          child: SizedBox(
+            height: size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: isMobile
+                      ? size.height * 0.5
+                      : isTablet
+                          ? size.height * 0.4
+                          : size.height * 0.3,
+                  child: const VerticalDivider(
+                    thickness: 1.5,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  'the new disruption',
+                  style: TextStyle(
+                    wordSpacing: 1.5,
+                    color: Colors.white,
+                    fontFamily: Fonts.autography,
+                    fontSize: isMobile
+                        ? 40.0
                         : isTablet
-                            ? 6
-                            : 5,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'the new disruption',
-                          style: TextStyle(
-                            wordSpacing: 1.5,
-                            color: Colors.white,
-                            fontFamily: Fonts.autography,
-                            fontSize: isMobile
-                                ? 34.0
-                                : isTablet
-                                    ? size.width * 0.07
-                                    : size.width * 0.06,
-                          ),
-                        ),
-                        Container(
-                          height: 1.0,
-                          color: Colors.white,
-                          width: double.infinity,
-                        ),
-                        SizedBox(height: isDesktop ? 40.0 : 20.0),
-                        RichText(
-                          text: TextSpan(
-                            text: 'Coming',
-                            style: TextStyle(
-                              wordSpacing: 1.5,
-                              color: Colors.orange,
-                              fontFamily: Fonts.mazzard,
-                              fontSize: isMobile
-                                  ? 32.0
-                                  : isTablet
-                                      ? size.width * 0.065
-                                      : size.width * 0.045,
-                            ),
-                            children: const [
-                              TextSpan(
-                                text: ' soon',
-                                style: TextStyle(fontStyle: FontStyle.italic),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                            ? size.width * 0.09
+                            : size.width * 0.06,
                   ),
-                  SizedBox(
-                    width: isDesktop ? size.width * 0.1 : 0.0,
-                    height: isDesktop ? 0.0 : size.height * 0.1,
+                ),
+                const SizedBox(height: 60.0),
+                SizedBox(
+                  height: 40.0,
+                  child: AnimatedTextKit(
+                    repeatForever: true,
+                    pause: const Duration(milliseconds: 200),
+                    animatedTexts: [
+                      FadeAnimatedText(
+                        'COMING SOON',
+                        textStyle: _textStyle,
+                        duration: const Duration(seconds: 4),
+                      ),
+                      FadeAnimatedText(
+                        'COMMUNITY  •  MAGAZINE',
+                        textStyle: _textStyle.copyWith(color: Colors.white),
+                        duration: const Duration(seconds: 4),
+                      ),
+                    ],
                   ),
-                  Flexible(
-                    flex: isMobile
-                        ? 0
-                        : isTablet
-                            ? 8
-                            : 3,
-                    child: const SocialHandles(),
-                  ),
-                ],
-              ),
-              const Positioned(bottom: 0.0, child: Footer()),
-            ],
+                ),
+              ],
+            ),
           ),
         );
       },
