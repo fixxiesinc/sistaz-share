9d0a351ab1edaf25ac6cb4588e1313cecfd3393e
Othniel Ussher <othnielussher16@gmail.com>
2022-01-16 13:57:35 +0000

    Redesign pages

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,91 +1,25 @@
-import 'dart:html' as html;
 import 'package:flutter/material.dart';
 import 'package:sistaz_share_web/exports.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  int currentPage = 0;
-  late List<Widget> pages = const [FirstBody(), SecondBody()];
-
-  @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.black,
-      body: ResponsiveBuilder(
-        builder: (context, sizingInformation) {
-          bool isMobile = sizingInformation.isMobile;
-          bool isTablet = sizingInformation.isTablet;
-          return SizedBox(
-            height: size.height,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            vertical: 20.0, horizontal: isMobile ? 20.0 : 40.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            InkWell(
-                              onTap: () => html.window.location.reload(),
-                              child: Image.asset(
-                                Images.logoTextWhite,
-                                width: isMobile
-                                    ? size.width * 0.35
-                                    : isTablet
-                                        ? size.width * 0.2
-                                        : size.width * 0.12,
-                              ),
-                            ),
-                            RotatedBox(
-                              quarterTurns: 1,
-                              child: DotsIndicator(
-                                dotsCount: pages.length,
-                                position: currentPage.toDouble(),
-                                decorator: DotsDecorator(
-                                  activeColor: Colors.white,
-                                  size: const Size.square(7.0),
-                                  activeSize: const Size.square(7.0),
-                                  color: Colors.grey.withOpacity(0.8),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: isMobile ? 20.0 : 10.0),
-                      Expanded(
-                        child: SizedBox(
-                          height: size.height,
-                          child: PageView.builder(
-                            itemCount: pages.length,
-                            clipBehavior: Clip.antiAlias,
-                            scrollDirection: Axis.vertical,
-                            itemBuilder: (context, index) => pages[index],
-                            onPageChanged: (value) {
-                              setState(() => currentPage = value);
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Header(),
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.only(top: 20.0),
+              children: const [FirstBody(), SecondBody()],
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
