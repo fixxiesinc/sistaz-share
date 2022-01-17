e19b69607cb5385781b591a459cc68b6a0ce6891
Othniel Ussher <othnielussher16@gmail.com>
2022-01-17 20:05:48 +0000

    Redesign UI

--- lib/screens/home/menu.dart
+++ lib/screens/home/menu.dart
@@ -7,54 +7,64 @@ class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return SizedBox(
-      height: size.height * 0.95,
-      child: ResponsiveBuilder(
-        builder: (context, sizingInformation) {
-          bool isMobile = sizingInformation.isMobile;
-          bool isTablet = sizingInformation.isTablet;
-          return Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: 30.0,
-              horizontal: isMobile ? 20.0 : 60.0,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        bool isMobile = sizingInformation.isMobile;
+        bool isTablet = sizingInformation.isTablet;
+        return Consumer<MenuProvider>(
+          builder: (context, provider, child) {
+            return SizedBox(
+              height: provider.menuOpen ? size.height : 0,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: isMobile ? 20.0 : 60.0,
+                  right: isMobile ? 20.0 : 60.0,
+                  bottom: 60.0,
+                  top: isMobile
+                      ? 20.0
+                      : isTablet
+                          ? 40.0
+                          : 60.0,
+                ),
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    RichText(
-                      text: TextSpan(
-                        text: 'Coming',
-                        style: TextStyle(
-                          color: Colors.orange,
-                          fontFamily: Fonts.mazzard,
-                          fontSize: isMobile
-                              ? 32.0
-                              : isTablet
-                                  ? size.width * 0.06
-                                  : size.width * 0.045,
-                        ),
-                        children: const [
-                          TextSpan(
-                            text: ' soon',
-                            style: TextStyle(fontStyle: FontStyle.italic),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: 'Coming',
+                            style: TextStyle(
+                              color: Colors.orange,
+                              fontFamily: Fonts.mazzard,
+                              fontSize: isMobile
+                                  ? 32.0
+                                  : isTablet
+                                      ? size.width * 0.06
+                                      : size.width * 0.045,
+                            ),
+                            children: const [
+                              TextSpan(
+                                text: ' soon',
+                                style: TextStyle(fontStyle: FontStyle.italic),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                        const SizedBox(height: 40.0),
+                        const SocialHandles(),
+                      ],
                     ),
-                    const SizedBox(height: 40.0),
-                    const SocialHandles(),
+                    const Footer(),
                   ],
                 ),
-                const Footer(),
-              ],
-            ),
-          );
-        },
-      ),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
