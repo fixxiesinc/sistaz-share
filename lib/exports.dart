b00001db5d7b44674a2e5b40421d8832d6d0f0e0
Othniel Ussher <othnielussher16@gmail.com>
2022-01-17 20:04:23 +0000

    🚢 Add new exports

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,4 +1,5 @@
 // pub
+export 'package:provider/provider.dart';
 export 'package:delayed_display/delayed_display.dart';
 export 'package:responsive_builder/responsive_builder.dart';
 export 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
@@ -10,9 +11,11 @@ export 'package:sistaz_share_web/screens/splash.dart';
 export 'package:sistaz_share_web/shared/constants.dart';
 export 'package:sistaz_share_web/screens/home/menu.dart';
 export 'package:sistaz_share_web/screens/home/home.dart';
+export 'package:sistaz_share_web/providers/menu_provider.dart';
 export 'package:sistaz_share_web/screens/home/social_handles.dart';
 export 'package:sistaz_share_web/screens/home/widgets/header.dart';
 export 'package:sistaz_share_web/screens/home/widgets/first_body.dart';
 export 'package:sistaz_share_web/screens/home/widgets/first_body.dart';
+export 'package:sistaz_share_web/screens/home/widgets/middle_line.dart';
 export 'package:sistaz_share_web/screens/home/widgets/second_body.dart';
 export 'package:sistaz_share_web/screens/home/widgets/social_handle_item.dart';
