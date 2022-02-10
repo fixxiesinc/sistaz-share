394263cfadc66bd8507dcc0b3aabb52cc89a021f
Othniel Ussher <othnielussher16@gmail.com>
2022-02-10 23:02:57 +0000

    Rename class

--- lib/screens/home/widgets/second_body.dart
+++ lib/screens/home/widgets/second_body.dart
@@ -1,75 +1,152 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:sistaz_share_web/exports.dart';
 
 class SecondBody extends StatelessWidget {
   const SecondBody({Key? key}) : super(key: key);
 
+  final String caption =
+      '''Sistaz Share publishes social, lifestyle and personal development content - that highlight stories relatable to the feminine endeavor''';
+
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
     return ResponsiveBuilder(
       builder: (context, sizingInformation) {
         final bool isMobile = sizingInformation.isMobile;
         final bool isTablet = sizingInformation.isTablet;
-        final _textStyle = TextStyle(
-          wordSpacing: 1.5,
-          letterSpacing: 1.5,
-          color: Colors.orange,
-          fontFamily: Fonts.mazzard,
-          fontSize: isMobile
-              ? 16.0
-              : isTablet
-                  ? size.width * 0.02
-                  : size.width * 0.014,
-        );
+        final bool isDesktop = sizingInformation.isDesktop;
         return Material(
           color: Colors.black,
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: isMobile ? 20.0 : 60.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            padding: EdgeInsets.only(
+              left: isMobile ? 20.0 : 60.0,
+              // right: isMobile ? 20.0 : 60.0,
+              bottom: 60.0,
+              top: isMobile
+                  ? 20.0
+                  : isTablet
+                      ? 40.0
+                      : 60.0,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(height: 60.0),
-                Text(
-                  'the new disruption',
-                  style: TextStyle(
-                    wordSpacing: 1.5,
-                    color: Colors.white,
-                    fontFamily: Fonts.autography,
-                    fontSize: isMobile
-                        ? 44.0
-                        : isTablet
-                            ? size.width * 0.09
-                            : size.width * 0.06,
-                  ),
-                ),
-                const SizedBox(height: 40.0),
-                SizedBox(
-                  height: 40.0,
-                  child: AnimatedTextKit(
-                    repeatForever: true,
-                    pause: const Duration(milliseconds: 200),
-                    animatedTexts: [
-                      FadeAnimatedText(
-                        'COMING SOON',
-                        textStyle: _textStyle,
-                        duration: const Duration(seconds: 3),
+                Expanded(
+                  flex: 6,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: const TextSpan(
+                          text: 'Our',
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.orange,
+                            fontFamily: Fonts.mazzard,
+                            fontStyle: FontStyle.italic,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: '  Editorial',
+                              style: TextStyle(fontStyle: FontStyle.normal),
+                            ),
+                          ],
+                        ),
                       ),
-                      FadeAnimatedText(
-                        'COMMUNITY  •  MAGAZINE',
-                        textStyle: _textStyle.copyWith(color: Colors.white),
-                        duration: const Duration(seconds: 3),
+                      const SizedBox(height: 30.0),
+                      Text(
+                        caption,
+                        style: const TextStyle(
+                          height: 1.3,
+                          fontSize: 18.0,
+                          color: Colors.white,
+                        ),
                       ),
                     ],
                   ),
                 ),
-                const SizedBox(height: 60.0),
+                const Spacer(),
+                const Expanded(flex: 6, child: Categories()),
               ],
             ),
           ),
         );
       },
+    );
+  }
+}
+
+class Categories extends StatefulWidget {
+  const Categories({Key? key}) : super(key: key);
+
+  @override
+  State<Categories> createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      height: size.height * 0.65,
+      child: ListView.builder(
+        controller: _scrollController,
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          Category category = categories[index];
+          return Padding(
+            padding: EdgeInsets.only(
+              right: index == categories.length - 1 ? 60.0 : 30.0,
+            ),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: size.height * 0.5),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    color: Colors.grey[900],
+                    width: size.height * 0.5,
+                    height: size.height * 0.4,
+                    child: category.image,
+                  ),
+                  const SizedBox(height: 20.0),
+                  Text(
+                    category.label,
+                    style: const TextStyle(
+                      fontSize: 22.0,
+                      letterSpacing: 1.3,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 12.0),
+                  Wrap(
+                    children: category.subCategories.map((subCategory) {
+                      return Text(
+                        subCategory,
+                        style: Theme.of(context)
+                            .textTheme
+                            .subtitle1!
+                            .copyWith(color: Colors.white),
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
