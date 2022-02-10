d76bc405aaf1a50764b508df55ce3a63c73e094e
Othniel Ussher <othnielussher16@gmail.com>
2022-02-10 23:06:37 +0000

    Move categories widget to separate class

--- lib/screens/home/widgets/second_body.dart
+++ lib/screens/home/widgets/second_body.dart
@@ -13,7 +13,6 @@ class SecondBody extends StatelessWidget {
       builder: (context, sizingInformation) {
         final bool isMobile = sizingInformation.isMobile;
         final bool isTablet = sizingInformation.isTablet;
-        final bool isDesktop = sizingInformation.isDesktop;
         return Material(
           color: Colors.black,
           child: Padding(
@@ -66,87 +65,12 @@ class SecondBody extends StatelessWidget {
                   ),
                 ),
                 const Spacer(),
-                const Expanded(flex: 6, child: Categories()),
+                const Expanded(flex: 6, child: StoryCategories()),
               ],
             ),
           ),
         );
       },
-    );
-  }
-}
-
-class Categories extends StatefulWidget {
-  const Categories({Key? key}) : super(key: key);
-
-  @override
-  State<Categories> createState() => _CategoriesState();
-}
-
-class _CategoriesState extends State<Categories> {
-  final ScrollController _scrollController = ScrollController();
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return SizedBox(
-      height: size.height * 0.65,
-      child: ListView.builder(
-        controller: _scrollController,
-        scrollDirection: Axis.horizontal,
-        itemCount: categories.length,
-        itemBuilder: (context, index) {
-          Category category = categories[index];
-          return Padding(
-            padding: EdgeInsets.only(
-              right: index == categories.length - 1 ? 60.0 : 30.0,
-            ),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: size.height * 0.5),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    color: Colors.grey[900],
-                    width: size.height * 0.5,
-                    height: size.height * 0.4,
-                    child: category.image,
-                  ),
-                  const SizedBox(height: 20.0),
-                  Text(
-                    category.label,
-                    style: const TextStyle(
-                      fontSize: 22.0,
-                      letterSpacing: 1.3,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(height: 12.0),
-                  Wrap(
-                    children: category.subCategories.map((subCategory) {
-                      return Text(
-                        subCategory,
-                        style: Theme.of(context)
-                            .textTheme
-                            .subtitle1!
-                            .copyWith(color: Colors.white),
-                      );
-                    }).toList(),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
     );
   }
 }
