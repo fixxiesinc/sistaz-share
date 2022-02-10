f42487d84c21bf91859fa05b12e2c1aa8cacfee6
Othniel Ussher <othnielussher16@gmail.com>
2022-02-10 23:56:37 +0000

    Fix padding

--- lib/screens/home/widgets/story_categories.dart
+++ lib/screens/home/widgets/story_categories.dart
@@ -20,58 +20,75 @@ class _StoryCategoriesState extends State<StoryCategories> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return SizedBox(
-      height: size.height * 0.65,
-      child: ListView.builder(
-        controller: _scrollController,
-        scrollDirection: Axis.horizontal,
-        itemCount: categories.length,
-        itemBuilder: (context, index) {
-          Category category = categories[index];
-          return Padding(
-            padding: EdgeInsets.only(
-              right: index == categories.length - 1 ? 60.0 : 30.0,
-            ),
-            child: ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: size.height * 0.5),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    color: Colors.grey[900],
-                    width: size.height * 0.5,
-                    height: size.height * 0.4,
-                    child: category.image,
-                  ),
-                  const SizedBox(height: 20.0),
-                  Text(
-                    category.label,
-                    style: const TextStyle(
-                      fontSize: 22.0,
-                      letterSpacing: 1.3,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(height: 12.0),
-                  Wrap(
-                    children: category.subCategories.map((subCategory) {
-                      return Text(
-                        subCategory,
-                        style: Theme.of(context)
-                            .textTheme
-                            .subtitle1!
-                            .copyWith(color: Colors.white),
-                      );
-                    }).toList(),
-                  ),
-                ],
+    return ResponsiveBuilder(builder: (context, sizingInformation) {
+      final bool isMobile = sizingInformation.isMobile;
+      return SizedBox(
+        height: isMobile ? size.height * 0.52 : size.height * 0.55,
+        child: ListView.builder(
+          controller: _scrollController,
+          scrollDirection: Axis.horizontal,
+          itemCount: categories.length,
+          itemBuilder: (context, index) {
+            Category category = categories[index];
+            return Padding(
+              padding: EdgeInsets.only(
+                left: index == 0
+                    ? isMobile
+                        ? 10.0
+                        : 60.0
+                    : 0,
+                right: isMobile
+                    ? 20.0
+                    : index == categories.length - 1
+                        ? 60.0
+                        : 30.0,
               ),
-            ),
-          );
-        },
-      ),
-    );
+              child: ResponsiveBuilder(builder: (context, sizingInformation) {
+                final bool isMobile = sizingInformation.isMobile;
+                return ConstrainedBox(
+                  constraints: BoxConstraints(
+                      maxWidth:
+                          isMobile ? size.width * 0.9 : size.height * 0.5),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: category.image,
+                        color: Colors.grey[900],
+                        height: size.height * 0.4,
+                        width: isMobile ? size.width * 0.9 : size.height * 0.5,
+                      ),
+                      const SizedBox(height: 20.0),
+                      Text(
+                        category.label,
+                        style: const TextStyle(
+                          fontSize: 22.0,
+                          letterSpacing: 1.3,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const SizedBox(height: 12.0),
+                      Wrap(
+                        children: category.subCategories.map((subCategory) {
+                          return Text(
+                            subCategory,
+                            style: Theme.of(context)
+                                .textTheme
+                                .subtitle1!
+                                .copyWith(color: Colors.white),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+            );
+          },
+        ),
+      );
+    });
   }
 }
