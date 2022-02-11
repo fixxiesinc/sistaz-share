f470a2dea280217dc5baca27188fd381b789ef06
Othniel Ussher <othnielussher16@gmail.com>
2022-02-11 22:33:09 +0000

    Update UI

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,5 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:sistaz_share_web/models/early_adopter.dart';
+import 'package:sistaz_share_web/exports.dart';
 
 class DatabaseService {
   static const String _earlyAdopters = 'early_adopters';
@@ -7,7 +6,13 @@ class DatabaseService {
   static final CollectionReference _adoptersCollection =
       FirebaseFirestore.instance.collection(_earlyAdopters);
 
-  addNewAdopter(EarlyAdopter earlyAdopter) async {
-    return await _adoptersCollection.add(earlyAdopter.toMap());
+  static Future<Map<String, dynamic>> addNewAdopter(
+      EarlyAdopter earlyAdopter) async {
+    try {
+      await _adoptersCollection.add(earlyAdopter.toMap());
+      return {'state': true};
+    } catch (exception) {
+      return {'state': false, 'error': exception.toString()};
+    }
   }
 }
