2d4a63296dd14acc03c464d1f52867a2a90a0199
Othniel Ussher <othnielussher16@gmail.com>
2022-02-11 21:16:39 +0000

    Update UI

--- lib/screens/home/widgets/story_categories.dart
+++ lib/screens/home/widgets/story_categories.dart
@@ -20,35 +20,39 @@ class _StoryCategoriesState extends State<StoryCategories> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return ResponsiveBuilder(builder: (context, sizingInformation) {
-      final bool isMobile = sizingInformation.isMobile;
-      return SizedBox(
-        height: isMobile ? size.height * 0.52 : size.height * 0.55,
-        child: ListView.builder(
-          controller: _scrollController,
-          scrollDirection: Axis.horizontal,
-          itemCount: categories.length,
-          itemBuilder: (context, index) {
-            Category category = categories[index];
-            return Padding(
-              padding: EdgeInsets.only(
-                left: index == 0
-                    ? isMobile
-                        ? 20.0
-                        : 60.0
-                    : 0,
-                right: isMobile
-                    ? 20.0
-                    : index == categories.length - 1
-                        ? 60.0
-                        : 30.0,
-              ),
-              child: ResponsiveBuilder(builder: (context, sizingInformation) {
-                final bool isMobile = sizingInformation.isMobile;
-                return ConstrainedBox(
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        final bool isMobile = sizingInformation.isMobile;
+        return ConstrainedBox(
+          constraints: BoxConstraints(
+            // minHeight: isMobile ? size.height * 0.52 : size.height * 0.55,
+            // maxHeight: isMobile ? size.height * 0.6 : size.height * 0.55,
+            minHeight: size.height * 0.6,
+            maxHeight: size.height * 0.7,
+          ),
+          child: ListView.builder(
+            controller: _scrollController,
+            scrollDirection: Axis.horizontal,
+            itemCount: categories.length,
+            itemBuilder: (context, index) {
+              Category category = categories[index];
+              return Padding(
+                padding: EdgeInsets.only(
+                  left: index == 0
+                      ? isMobile
+                          ? 20.0
+                          : 60.0
+                      : 0,
+                  right: isMobile
+                      ? 20.0
+                      : index == categories.length - 1
+                          ? 60.0
+                          : 30.0,
+                ),
+                child: ConstrainedBox(
                   constraints: BoxConstraints(
-                      maxWidth:
-                          isMobile ? size.width * 0.9 : size.height * 0.5),
+                    maxWidth: isMobile ? size.width * 0.9 : size.height * 0.5,
+                  ),
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -71,24 +75,26 @@ class _StoryCategoriesState extends State<StoryCategories> {
                       ),
                       const SizedBox(height: 12.0),
                       Wrap(
-                        children: category.subCategories.map((subCategory) {
-                          return Text(
-                            subCategory,
-                            style: Theme.of(context)
-                                .textTheme
-                                .subtitle1!
-                                .copyWith(color: Colors.white),
-                          );
-                        }).toList(),
+                        children: category.subCategories.map(
+                          (subCategory) {
+                            return Text(
+                              subCategory,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle1!
+                                  .copyWith(color: Colors.white),
+                            );
+                          },
+                        ).toList(),
                       ),
                     ],
                   ),
-                );
-              }),
-            );
-          },
-        ),
-      );
-    });
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
   }
 }
