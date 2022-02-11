a99aadf63da2f7a1ecb7ea381f3981aaa163ba6f
Othniel Ussher <othnielussher16@gmail.com>
2022-02-11 21:16:16 +0000

    Update UI

--- lib/screens/home/menu.dart
+++ lib/screens/home/menu.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:sistaz_share_web/exports.dart';
 
-class Menu extends StatelessWidget {
+class Menu extends StatefulWidget {
   const Menu({Key? key}) : super(key: key);
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,42 +33,60 @@ class Menu extends StatelessWidget {
                   left: isMobile ? 20.0 : 60.0,
                   right: isMobile ? 20.0 : 60.0,
                   bottom: 60.0,
-                  top: isMobile
-                      ? 20.0
-                      : isTablet
-                          ? 40.0
-                          : 60.0,
                 ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        RichText(
-                          text: TextSpan(
-                            text: 'Coming',
-                            style: TextStyle(
-                              color: Colors.orange,
-                              fontFamily: Fonts.mazzard,
-                              fontSize: isMobile
-                                  ? 32.0
-                                  : isTablet
-                                      ? size.width * 0.06
-                                      : size.width * 0.045,
-                            ),
-                            children: const [
-                              TextSpan(
-                                text: ' soon',
-                                style: TextStyle(fontStyle: FontStyle.italic),
+                    SingleChildScrollView(
+                      controller: _scrollController,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: isMobile
+                                ? 20.0
+                                : isTablet
+                                    ? 40.0
+                                    : Get.height * 0.05,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              RichText(
+                                text: TextSpan(
+                                  text: 'Coming',
+                                  style: TextStyle(
+                                    color: Colors.orange,
+                                    fontFamily: Fonts.mazzard,
+                                    fontSize: isMobile
+                                        ? 32.0
+                                        : isTablet
+                                            ? size.width * 0.06
+                                            : size.width * 0.045,
+                                  ),
+                                  children: const [
+                                    TextSpan(
+                                      text: ' soon',
+                                      style: TextStyle(
+                                          fontStyle: FontStyle.italic),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(height: 40.0),
+                              const SocialHandles(),
+                              const SizedBox(height: 40.0),
+                              FilledButton(
+                                label: 'READ & TELL STORIES',
+                                onPressed: () => Get.to(
+                                  () => const TellStories(),
+                                  routeName: 'read-and-tell-your-stories',
+                                ),
                               ),
                             ],
                           ),
-                        ),
-                        const SizedBox(height: 40.0),
-                        const SocialHandles(),
-                      ],
+                        ],
+                      ),
                     ),
                     const Footer(),
                   ],
