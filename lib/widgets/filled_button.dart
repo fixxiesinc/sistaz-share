057d1f6c18695372bad98c765424c26720c0a038
Othniel Ussher <othnielussher16@gmail.com>
2022-02-11 21:17:04 +0000

    Update UI

---
+++ lib/widgets/filled_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class FilledButton extends StatelessWidget {
+  final String label;
+  final VoidCallback? onPressed;
+  const FilledButton({Key? key, required this.label, required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(
+        label,
+        style: Theme.of(context)
+            .textTheme
+            .button!
+            .copyWith(fontWeight: FontWeight.w600),
+      ),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color?>(Colors.white),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          const RoundedRectangleBorder(
+            borderRadius: BorderRadius.zero,
+          ),
+        ),
+        padding: MaterialStateProperty.resolveWith<EdgeInsets?>(
+          (Set<MaterialState> states) {
+            return const EdgeInsets.symmetric(
+              vertical: 20.0,
+              horizontal: 30.0,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
