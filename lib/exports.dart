5e1fa24ca21378b0a461211bb7d1c919b14802b9
Othniel Ussher <othnielussher16@gmail.com>
2022-02-11 21:15:57 +0000

    🚢 Update exports

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,7 +1,14 @@
 // pub
+export 'package:get/get.dart';
+export 'package:demoji/demoji.dart';
 export 'package:provider/provider.dart';
+export 'package:dots_indicator/dots_indicator.dart';
+export 'package:email_validator/email_validator.dart';
 export 'package:delayed_display/delayed_display.dart';
+export 'package:transparent_image/transparent_image.dart';
+export 'package:animated_text_kit/animated_text_kit.dart';
 export 'package:responsive_builder/responsive_builder.dart';
+export 'package:argon_buttons_flutter/argon_buttons_flutter.dart';
 export 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
 
 // local
@@ -12,13 +19,20 @@ export 'package:sistaz_share_web/models/category.dart';
 export 'package:sistaz_share_web/shared/constants.dart';
 export 'package:sistaz_share_web/screens/home/menu.dart';
 export 'package:sistaz_share_web/screens/home/home.dart';
+export 'package:sistaz_share_web/widgets/filled_button.dart';
+export 'package:sistaz_share_web/widgets/required_button.dart';
 export 'package:sistaz_share_web/providers/menu_provider.dart';
+export 'package:sistaz_share_web/providers/page_provider.dart';
+export 'package:sistaz_share_web/screens/home/tell_stories.dart';
+export 'package:sistaz_share_web/screens/home/get_notified.dart';
 export 'package:sistaz_share_web/screens/home/social_handles.dart';
 export 'package:sistaz_share_web/screens/home/widgets/header.dart';
 export 'package:sistaz_share_web/screens/home/widgets/first_body.dart';
 export 'package:sistaz_share_web/screens/home/widgets/first_body.dart';
 export 'package:sistaz_share_web/screens/home/widgets/third_body.dart';
+export 'package:sistaz_share_web/screens/home/widgets/fourth_body.dart';
 export 'package:sistaz_share_web/screens/home/widgets/second_body.dart';
 export 'package:sistaz_share_web/screens/home/widgets/page_divider.dart';
 export 'package:sistaz_share_web/screens/home/widgets/story_categories.dart';
 export 'package:sistaz_share_web/screens/home/widgets/social_handle_item.dart';
+export 'package:sistaz_share_web/screens/home/widgets/become_a_contributor.dart';
