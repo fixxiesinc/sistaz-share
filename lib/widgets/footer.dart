4c2a67bc7e6310e2fdfe4d4597dcfe6c07c7573e
Othniel Ussher <othnielussher16@gmail.com>
2022-02-11 21:17:07 +0000

    Update UI

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -12,46 +12,13 @@ class Footer extends StatelessWidget {
         builder: (context, sizingInformation) {
           return Padding(
             padding: const EdgeInsets.symmetric(vertical: 60.0),
-            child: FittedBox(
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.asset(Images.logoIconWhite, height: 20.0),
-                  const SizedBox(width: 12.0),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'COPYRIGHT SISTAZ SHARE ${DateTime.now().year}',
-                        style: const TextStyle(
-                          fontSize: 8.0,
-                          wordSpacing: 1.6,
-                          letterSpacing: 1.3,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(width: 12.0),
-                      Container(
-                        width: 4.0,
-                        height: 4.0,
-                        decoration: const BoxDecoration(
-                          color: Colors.white,
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(width: 12.0),
-                  const Text(
-                    'FROM FIXXIES',
-                    style: TextStyle(
-                      fontSize: 8.0,
-                      wordSpacing: 1.6,
-                      color: Colors.white,
-                      letterSpacing: 1.3,
-                    ),
-                  ),
-                ],
+            child: Text(
+              'ALL RIGHTS RESERVED SISTAZ SHARE ${DateTime.now().year}',
+              style: const TextStyle(
+                fontSize: 10.0,
+                wordSpacing: 1.6,
+                letterSpacing: 1.3,
+                color: Colors.grey,
               ),
             ),
           );
