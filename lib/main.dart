e2a52169b3ac9e3309b5e2f9fbad4f9889fe2430
Othniel Ussher <othnielussher16@gmail.com>
2022-02-11 22:33:01 +0000

    Add firebase config data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sistaz_share_web/exports.dart';
+import 'package:sistaz_share_web/firebase_config.dart';
 
-void main() => runApp(const App());
+void main() async {
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp(
+        options: const FirebaseOptions(
+          apiKey: FirebaseConfig.apiKey,
+          appId: FirebaseConfig.appId,
+          projectId: FirebaseConfig.projectId,
+          messagingSenderId: FirebaseConfig.messagingSenderId,
+        ),
+      );
+      runApp(const App());
+    },
+    (error, stackTrace) {
+      return;
+    },
+  );
+}
