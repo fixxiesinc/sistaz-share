068146b65067947ae3689298d15d0913b554d7e9
Othniel Ussher <othnielussher16@gmail.com>
2022-02-13 10:54:16 +0000

    Adjust spacing across entire site

--- lib/screens/home/widgets/second_body.dart
+++ lib/screens/home/widgets/second_body.dart
@@ -14,69 +14,41 @@ class SecondBody extends StatelessWidget {
       child: ResponsiveBuilder(
         builder: (context, sizingInformation) {
           final bool isMobile = sizingInformation.isMobile;
-          final bool isTablet = sizingInformation.isTablet;
           final bool isDesktop = sizingInformation.isDesktop;
-
-          
-          return Padding(
-            padding: EdgeInsets.only(
-              top: isMobile
-                  ? 20.0
-                  : isTablet
-                      ? 40.0
-                      : 0.0,
-            ),
-            child: Flex(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              direction: isDesktop ? Axis.horizontal : Axis.vertical,
-              children: [
-                Expanded(
-                  flex: isDesktop ? 6 : 0,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: isMobile ? 20.0 : 60.0,
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        RichText(
-                          text: const TextSpan(
-                            text: 'Our',
-                            style: TextStyle(
-                              fontSize: 30.0,
-                              color: Colors.orange,
-                              fontFamily: Fonts.mazzard,
-                              fontStyle: FontStyle.italic,
-                            ),
-                            children: [
-                              TextSpan(
-                                text: '  Editorial',
-                                style: TextStyle(fontStyle: FontStyle.normal),
-                              ),
-                            ],
-                          ),
+          return Flex(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            direction: isDesktop ? Axis.horizontal : Axis.vertical,
+            children: [
+              Expanded(
+                flex: isDesktop ? 6 : 0,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: isMobile ? 20.0 : 60.0,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          text: caption,
+                          style:
+                              Theme.of(context).textTheme.headline4!.copyWith(
+                                    color: Colors.orange,
+                                    fontFamily: Fonts.mazzard,
+                                  ),
                         ),
-                        SizedBox(height: isMobile ? 20.0 : 30.0),
-                        Text(
-                          caption,
-                          style: const TextStyle(
-                            height: 1.3,
-                            fontSize: 18.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: isMobile ? 20.0 : 30.0),
-                Expanded(
-                  flex: isDesktop ? 6 : 0,
-                  child: const StoryCategories(),
-                ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 60.0),
+              Expanded(
+                flex: isDesktop ? 6 : 0,
+                child: const StoryCategories(),
+              ),
+            ],
           );
         },
       ),
