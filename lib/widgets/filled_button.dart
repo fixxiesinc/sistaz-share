bb8caeff50a8cbb2a181eeda4ceb5f87f156edae
Othniel Ussher <othnielussher16@gmail.com>
2022-02-15 18:00:02 +0000

    Update button sizes

--- lib/widgets/filled_button.dart
+++ lib/widgets/filled_button.dart
@@ -12,29 +12,27 @@ class FilledButton extends StatelessWidget {
     return ResponsiveBuilder(
       builder: (context, sizingInformation) {
         bool isDesktop = sizingInformation.isDesktop;
-        return ElevatedButton(
-          onPressed: onPressed,
-          child: Text(
-            label,
-            style: Theme.of(context)
-                .textTheme
-                .button!
-                .copyWith(letterSpacing: 0.5),
-          ),
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color?>(Colors.white),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              const RoundedRectangleBorder(
-                borderRadius: BorderRadius.zero,
-              ),
+        return SizedBox(
+          height: isDesktop ? 43 : 38,
+          child: ElevatedButton(
+            onPressed: onPressed,
+            child: Text(
+              label,
+              style: Theme.of(context)
+                  .textTheme
+                  .button!
+                  .copyWith(letterSpacing: 0.5),
             ),
-            padding: MaterialStateProperty.resolveWith<EdgeInsets?>(
-              (Set<MaterialState> states) {
-                return EdgeInsets.symmetric(
-                  horizontal: 30.0,
-                  vertical: isDesktop ? 22.0 : 20.0,
-                );
-              },
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color?>(Colors.white),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+              ),
+              padding: MaterialStateProperty.resolveWith<EdgeInsets?>(
+                (Set<MaterialState> states) {
+                  return const EdgeInsets.symmetric(horizontal: 30.0);
+                },
+              ),
             ),
           ),
         );
